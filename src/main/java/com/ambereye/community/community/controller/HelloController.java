a462fda1ccb4ee64e8b6a2c781bbf4e40dd4250f
ambereye <287914967@qq.com>
2019-09-14 00:09:48 +0800

    Spring Boot项目初启动

---
+++ src/main/java/com/ambereye/community/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.ambereye.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * TODO
+ *
+ * @author ambereye
+ * @date 2019/9/13
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")  String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
