2bf91895dc8256394d74644785ed7e7dd2e8eb60
ambereye <287914967@qq.com>
2019-09-14 12:50:15 +0800

    修改添加properties设置,解耦

--- src/main/java/com/ambereye/community/controller/AuthorizeController.java
+++ src/main/java/com/ambereye/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.ambereye.community.dto.AccessTokenDTO;
 import com.ambereye.community.dto.GithubUser;
 import com.ambereye.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,14 +21,22 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name="state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("0c7fd2ce173a7cb73f4e");
-        accessTokenDTO.setClient_secret("e5d6dd56b2056de171a9fd197b8d7c84b510b6de");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUrl);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
