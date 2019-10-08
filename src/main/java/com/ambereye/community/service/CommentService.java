56d7ebc9997a71b58cca4d3b2bcc9567ae255809
ambereye <287914967@qq.com>
2019-10-08 22:56:36 +0800

    add default login show

--- src/main/java/com/ambereye/community/service/CommentService.java
+++ src/main/java/com/ambereye/community/service/CommentService.java
@@ -80,6 +80,7 @@ public class CommentService {
             if (question == null) {
                 throw new CustomizeException(CustomizeErrorCodeEnum.QUESTION_NOT_FUND);
             }
+            comment.setCommentCount(0);
             commentMapper.insert(comment);
             question.setCommentCount(1);
             questionExtMapper.incCommentCount(question);
@@ -90,6 +91,9 @@ public class CommentService {
     }
 
     private void createNotify(Comment comment, Long receiver, String notifierName, String outerTitle, NotificationTypeEnum notificationType, Long outerId) {
+        if (receiver.equals(comment.getCommentator())) {
+            return;
+        }
         Notification notification = new Notification();
         notification.setGmtCreate(System.currentTimeMillis());
         notification.setType(notificationType.getType());
